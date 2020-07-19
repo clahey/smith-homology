9bed571102b930cc0b8190f8a8b272413cc22183
clahey <clahey@clahey.net>
2020-07-19 15:49:51 -0400

    Recreating simplex.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatrixComponent } from './matrix/matrix.component';
 import { SimplexComponent } from './simplex/simplex.component';
+import { TwoSimplexComponent } from './two-simplex/two-simplex.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MatrixComponent, SimplexComponent ],
+  declarations: [ AppComponent, HelloComponent, MatrixComponent, SimplexComponent, TwoSimplexComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/two-simplex/two-simplex.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-two-simplex',
+  templateUrl: './two-simplex.component.html',
+  styleUrls: ['./two-simplex.component.css']
+})
+export class TwoSimplexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
